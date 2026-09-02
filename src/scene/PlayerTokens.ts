b16092f6ba1480f57b211ba59581c79f2ed8b326
Ignasi <ignasimgol@gmail.com>
2026-09-02 11:09:12 +0200

    epa

--- src/scene/PlayerTokens.ts
+++ src/scene/PlayerTokens.ts
@@ -224,6 +224,8 @@ export class PlayerTokens {
 
   private readonly handlePointerDown = (event: PointerEvent): void => {
     if (this.domElement.classList.contains('is-dragging')) return;
+    if (this.domElement.classList.contains('is-annotating')) return;
+    if (this.domElement.classList.contains('is-moving')) return;
     this.setPointer(event);
     this.raycaster.setFromCamera(this.pointer, this.camera);
     const intersections = this.raycaster.intersectObjects([...this.tokens.values()].map((record) => record.mesh));
